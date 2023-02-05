a9cf1c49da05fb6514b60d509a82a02b2d1cb979
olexa <olexakljus@gmail.com>
2023-02-05 14:22:19 +0200

    menu templ

--- main_page/views.py
+++ main_page/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from .models import Category, Dish
 
 
 def main(request):
     categories = Category.objects.all()
-    dishes = Dish.objects.all()
+    # повертаємо за допомогою функції рендер через HTML файл (створили файл-шаблон menu.html в папці templates)
+    # в context записуємо словник,до якого зможемо звертатися в шаблоні
+    return render(request, 'menu.html', context={
+        'categories': categories,
+    })
 
-    for item in categories:
-        for dish in item:
-            print(dish)
-
-    res1 = f"Categories - {'; '.join(map(str, categories))}"
-    res2 = f"Dishes - {'; '.join(map(str, dishes))}"
-    return HttpResponse(
-        res1 + '    ///     ' + res2
-    )
+    # dishes = Dish.objects.all()
+    # або просто повертаємо все як респонс
+    # res1 = f"Categories - {'; '.join(map(str, categories))}"
+    # res2 = f"Dishes - {'; '.join(map(str, dishes))}"
+    # return HttpResponse(
+    #     res1 + '    ///     ' + res2
+    # )
